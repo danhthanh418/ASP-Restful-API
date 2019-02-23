4540fa143d14e938c985c716a605a87dd62afabd
Thanh Danh <15520798@gm.uit.edu.vn>
2019-02-23 12:54:31 +0700

    asp net api with Get, Post, Put, Delete

---
+++ OnlineShop/Controller/DanhMucController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OnlineShop.Controller
+{
+    public class DanhMucController : ApiController
+    {
+        [HttpGet]
+        public List<DanhMuc> LayToanBoDanhMuc()
+        {
+            CSDLTestDataContext context = new CSDLTestDataContext();
+            List<DanhMuc> dsDM = context.DanhMucs.ToList();
+            foreach(DanhMuc dm in dsDM)
+            {
+                dm.SanPhams.Clear();
+            }
+            return dsDM;
+        }
+
+        [HttpGet]
+        public DanhMuc DoanhMucTheoMa(int id)
+        {
+            CSDLTestDataContext context = new CSDLTestDataContext();
+            DanhMuc dm = context.DanhMucs.FirstOrDefault(x => x.MaDM == id);
+            if (dm != null)
+            {
+                dm.SanPhams.Clear();
+            }
+            return dm;
+        }
+
+        [HttpPost]
+        public bool LuuDanhMuc(int maDM, string tenDM)
+        {
+            try
+            {
+                CSDLTestDataContext context = new CSDLTestDataContext();
+                DanhMuc dm = new DanhMuc();
+                dm.MaDM = maDM;
+                dm.TenDM = tenDM;
+ 
+                context.DanhMucs.InsertOnSubmit(dm);
+                context.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+
+            }
+            return false;
+        }
+
+        [HttpPut]
+        public bool SuaDanhMuc(int maDM, string tenDM)
+        {
+            try
+            {
+                CSDLTestDataContext context = new CSDLTestDataContext();
+                DanhMuc dm = context.DanhMucs.FirstOrDefault(x => x.MaDM == maDM);
+                if (dm != null)
+                {
+                    dm.TenDM = tenDM;
+                    context.SubmitChanges();
+                    return true;
+                }
+            }
+            catch
+            {
+
+            }
+            return false;
+        }
+
+        [HttpDelete]
+
+        public bool XoaDanhMuc(int maDM)
+        {
+            try
+            {
+                CSDLTestDataContext context = new CSDLTestDataContext();
+                DanhMuc dm = context.DanhMucs.FirstOrDefault(x => x.MaDM == maDM);
+                if (dm != null)
+                {
+                    if (dm.SanPhams.Count > 0) return false;
+                    context.DanhMucs.DeleteOnSubmit(dm);
+                    context.SubmitChanges();
+                    return true;
+                }
+            }
+            catch
+            {
+
+            }
+            return false;
+        }
+    }
+}
---
+++ OnlineShop/Controller/SanPhamController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OnlineShop.Controller
+{
+    public class SanPhamController : ApiController
+    {
+        [HttpGet]
+        public List<SanPham> LayToanBoSanPham()
+        {
+            CSDLTestDataContext context = new CSDLTestDataContext();
+            List<SanPham> dsSP = context.SanPhams.ToList();
+            //khử đệ quy
+            foreach(SanPham sp in dsSP)
+            {
+                sp.DanhMuc = null;
+            }
+            return dsSP;
+        }
+
+        [HttpGet]
+        public SanPham SanPhamTheoMa(int id)
+        {
+            CSDLTestDataContext context = new CSDLTestDataContext();
+            SanPham sp = context.SanPhams.FirstOrDefault(x => x.MaSP == id);
+            if (sp != null)
+            {
+                sp.DanhMuc = null;
+            }
+            //khử đệ quy
+            return sp;
+        }
+
+        [HttpGet]
+
+        public List<SanPham> TimSanPhamTrongKhoangDonGia(int bd, int kt)
+        {
+            CSDLTestDataContext context = new CSDLTestDataContext();
+            List<SanPham> dsSP = context.SanPhams.Where(x =>x.DonGia >= bd && x.DonGia <= kt).ToList();
+            foreach(SanPham sp in dsSP)
+            {
+                sp.DanhMuc = null;
+            }
+            return dsSP;
+        }
+
+        [HttpPost]
+        public bool LuuSanPham (int maSP, string tenSP, int donGia, int danhMuc)
+        {
+            try
+            {
+                CSDLTestDataContext context = new CSDLTestDataContext();
+                SanPham sp = new SanPham();
+                sp.MaSP = maSP;
+                sp.TenSP = tenSP;
+                sp.DonGia = donGia;
+                sp.DanhMuc = danhMuc;
+
+                context.SanPhams.InsertOnSubmit(sp);
+                context.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+
+            }
+            return false;
+        }
+
+        [HttpPut]
+        public bool SuaSanPham(int maSP, string tenSP, int donGia, int danhMuc)
+        {
+            
+            try
+            {
+                CSDLTestDataContext context = new CSDLTestDataContext();
+                SanPham sp = context.SanPhams.FirstOrDefault(x => x.MaSP == maSP);
+                if(sp != null)
+                {
+                    sp.TenSP = tenSP;
+                    sp.DonGia = donGia;
+                    sp.DanhMuc = danhMuc;
+                    context.SubmitChanges();
+                    return true;
+                }
+               
+            }
+            catch
+            {
+
+            }
+            return false;
+        }
+
+        [HttpDelete]
+
+        public bool XoaSanPham(int maSP)
+        {
+            try
+            {
+                CSDLTestDataContext context = new CSDLTestDataContext();
+                SanPham sp = context.SanPhams.FirstOrDefault(x => x.MaSP == maSP);
+                if (sp != null)
+                {
+                    context.SanPhams.DeleteOnSubmit(sp);
+                    context.SubmitChanges();
+                    return true;
+                }
+            }
+            catch
+            {
+
+            }
+            return false;
+        }
+    }
+}
